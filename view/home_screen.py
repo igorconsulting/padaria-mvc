d281dc260223e7a12d76ee42b5e75c74cdd66e11
Igor-personal <icaetanodiniz@gmail.com>
2025-03-30 18:20:24 -0300

    add view refactoring

--- view/home_screen.py
+++ view/home_screen.py
@@ -1,16 +1,26 @@
 from display_builder import Display
 
 class HomeScreen(Display):
-    def __init__(self)->str:
-        self.__clear()
-
+    def __init__(self):
+        """
+        Initializes the HomeScreen display.
+        """
+        super().__init__()
+        
+    def display(self) -> str:
+        """
+        Displays the home screen menu and gets the user's command.
+        
+        Returns:
+            str: The user's command input.
+        """
         message = '''
         Sistema Bakery-Fast
 
         * Cadastrar Produto - 1
-        * Cadastrar Client - 2
-        * Buscar Client por Nome - 3
-        * Buscar Client por Estado  - 4
+        * Cadastrar Cliente - 2
+        * Buscar Cliente por Nome - 3
+        * Buscar Cliente por Estado  - 4
         * Cadastrar Produto - 5
         * Buscar Produto por Nome - 6
         * Buscar Produto por Sabor - 7
@@ -19,8 +29,6 @@ class HomeScreen(Display):
         * Sair - 0
         '''
 
-        print(message)
-
-        command = input("\n Insira o comando:")
-
+        self.show_message(message)
+        command = input("\nInsira o comando:")
         return command
